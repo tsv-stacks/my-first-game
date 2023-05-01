62f862a15f1ea20dc17e14bc2ef9e1832a03e543
tsv-stacks <tsvirdi@yahoo.co.uk>
2023-05-02 00:38:08 +0100

    new branch

---
+++ assets/scripts/background-animation.js
@@ -0,0 +1,8 @@
+const backgroundImage1 = new Image();
+backgroundImage1.src = "../background/back.png";
+const backgroundImage2 = new Image();
+backgroundImage2.src = "../background/middle.png";
+const backgroundImage3 = new Image();
+backgroundImage3.src = "../background/foreground.png";
+const backgroundImage3alt = new Image();
+backgroundImage3.src = "../background/foreground-empty.png";
